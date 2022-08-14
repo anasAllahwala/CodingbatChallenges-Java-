12bb84ed6c4109f99f399b30db947f1fd765daf3
AnasTheAnalyst <84326568+anasAllahwala@users.noreply.github.com>
2022-08-14 15:28:50 +0500

    Create withoutTen.java

---
+++ Java/Array-2/withoutTen.java
@@ -0,0 +1,30 @@
+/*Return a version of the given array where all the 10's have been removed. The remaining elements should shift left towards the start of the array as needed, and the empty spaces a the end of the array should be 0. So {1, 10, 10, 2} yields {1, 2, 0, 0}. You may modify and return the given array or make a new array.
+
+
+withoutTen([1, 10, 10, 2]) → [1, 2, 0, 0]
+withoutTen([10, 2, 10]) → [2, 0, 0]
+withoutTen([1, 99, 10]) → [1, 99, 0]
+*/
+
+
+public int[] withoutTen(int[] nums) {
+  int arr [] = new int[nums.length];
+  for(int i = 0; i<nums.length; i++){
+    if(nums[i]!=10){
+      arr[i]=nums[i];
+    }
+    }
+  int temp = 0;
+  for(int i = 0; i<arr.length; i++){
+    if(arr[i]==0){
+      for(int j=i; j<arr.length;j++){
+        if(arr[j]!=0){
+          arr[i]=arr[j];
+          arr[j]=0;
+          break;
+        }
+      }
+    }
+  }
+  return arr;
+}
